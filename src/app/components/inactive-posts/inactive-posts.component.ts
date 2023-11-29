b30372486c9a6e520fb1a439e1a8d42d76cd63e8
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-11-29 15:13:11 +0100

    initial commit

---
+++ src/app/components/inactive-posts/inactive-posts.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/service/posts.service';
+
+@Component({
+  selector: 'app-inactive-posts',
+  templateUrl: './inactive-posts.component.html',
+  styleUrls: ['./inactive-posts.component.scss']
+})
+export class InactivePostsComponent implements OnInit {
+
+  posts: Post[] = []
+
+  constructor(private postSrv: PostsService) { 
+    this.postSrv.recuperaPosts().then((element) =>{
+      this.posts = element
+      console.log("Posts:", this.posts)
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
